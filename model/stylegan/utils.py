394a984bb8aa84f97b4a17659d26c6235159de45
therealjtgill <jtgill@email.arizona.edu>
2020-03-30 21:56:39 -0700

    Moved explicit discriminator/generator evaluations from stylegan class to deprecated.py because I'm worried I'll need them later.

--- model/stylegan/utils.py
+++ model/stylegan/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 def scale_and_shift_pixels(image_in):
-    image_out = np.array(2.*image_in/255. - 1, dtype=np.float32)
-    return image_out
+   image_out = np.array(2.*image_in/255. - 1, dtype=np.float32)
+   return image_out
